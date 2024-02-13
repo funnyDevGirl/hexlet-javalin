70875bb876ac008afb2227a910b5ef19d2766b2b
alina tarasova <alishka.tarasova@gmail.com>
2024-02-13 17:21:01 +0300

    Added Course.java

---
+++ HexletJavalin/src/main/java/org/example/hexlet/model/Course.java
@@ -0,0 +1,33 @@
+package org.example.hexlet.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public final class Course {
+    private Long id;
+
+    @ToString.Include
+    private String name;
+    private String description;
+
+    public Course(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
